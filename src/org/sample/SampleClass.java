7850a9181d288938889a1eb0c94bafe2127290c1
Sabasiva2916 <sabasiva2916@gmail.com>
2024-07-11 13:17:08 +0530

    reverse of String

--- src/org/sample/SampleClass.java
+++ src/org/sample/SampleClass.java
@@ -3,5 +3,16 @@ package org.sample;
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		// Reverse of String
+		// String s ="java" output --> avaj
+		String s = "java";
+		String output = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char c = s.charAt(i);
+			output = output + c;
+		}
+		System.out.println("Reverse of String :" + output);
+
 	}
 }
